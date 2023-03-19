d5a960a9167a9052fe4c27c3254ee397c032cc75
Galina Ry <galya.rybynok@gmail.com>
2023-03-19 15:27:17 +0300

    hw4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,40 @@
 function Student(name, gender, age) {
-  
+    this.name = name
+    this.gender = gender
+    this.age = age
+    this.marks = []
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (!this.wasExcl) {
+        for (let mark of marks) {
+        parseInt(mark)
+        this.marks.push(mark)
+        }
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.wasExcl) {
+        return 0
+    } else {
+        return this.marks.reduce((acc, num, ind, arr) => {
+            acc += num
+            if(ind === arr.length - 1) {
+                return acc / arr.length
+            }
+            return acc
+        }, 0)
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    this.excluded = reason
+    this.wasExcl = true
+    delete this.marks
+    delete this.subject
 }
